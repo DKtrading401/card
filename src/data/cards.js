713f70380b7161260ab33fad824cb64ee7228dd3
dktrading401 <dktrading401@gmail.com>
2026-06-08 18:20:12 +0900

    Business Card React Version 0.1

--- src/data/cards.js
+++ src/data/cards.js
@@ -2,6 +2,8 @@ export const cards = {
 
   "dk.kwag": {
 
+    slug: "dk.kwag",
+
     company: "DK Holding GmbH",
 
     name: "Dongkwon Kwag",
@@ -9,33 +11,101 @@ export const cards = {
     title: "CEO / Geschäftsführer",
 
     office: "+49 (0)6258 992 6526",
-
     officeLink: "+4962589926526",
 
     mobile: "+49 (0)176 7263 2504",
-
     mobileLink: "+4917672632504",
 
     email: "dk.kwag@dk-networks.de",
 
     website: "https://www.dk-networks.de",
-
     websiteText: "www.dk-networks.de",
-
-    map:
-      "https://maps.google.com/?q=Robert-Bunsen-Straße+41+64579+Gernsheim+Germany",
 
     address: [
       "Robert-Bunsen-Straße 41",
       "64579 Gernsheim, Germany"
     ],
 
-    qr:
-      "/assets/employees/dk.kwag/qr.png",
+    map:
+      "https://maps.google.com/?q=Robert-Bunsen-Straße+41+64579+Gernsheim+Germany",
+
+    qr: "/assets/employees/dk.kwag/qr.png",
+
+    vcf: "/assets/employees/dk.kwag/dk.kwag.vcf"
+  },
+
+
+
+  "max-mustermann": {
+
+    slug: "max-mustermann",
+
+    company: "DK Holding GmbH",
+
+    name: "Max Mustermann",
+
+    title: "Sales Manager",
+
+    office: "+49 (0)6258 992 6500",
+    officeLink: "+4962589926500",
+
+    mobile: "+49 (0)170 123 4567",
+    mobileLink: "+491701234567",
+
+    email: "max.mustermann@dk-networks.de",
+
+    website: "https://www.dk-networks.de",
+    websiteText: "www.dk-networks.de",
+
+    address: [
+      "Robert-Bunsen-Straße 41",
+      "64579 Gernsheim, Germany"
+    ],
+
+    map:
+      "https://maps.google.com/?q=Robert-Bunsen-Straße+41+64579+Gernsheim+Germany",
+
+    qr: "/assets/employees/max-mustermann/qr.png",
 
     vcf:
-      "/assets/employees/dk.kwag/dk.kwag.vcf"
+      "/assets/employees/max-mustermann/max-mustermann.vcf"
+  },
 
+
+
+  "john-doe": {
+
+    slug: "john-doe",
+
+    company: "DK Holding GmbH",
+
+    name: "John Doe",
+
+    title: "Project Manager",
+
+    office: "+49 (0)6258 992 6501",
+    officeLink: "+4962589926501",
+
+    mobile: "+49 (0)171 987 6543",
+    mobileLink: "+491719876543",
+
+    email: "john.doe@dk-networks.de",
+
+    website: "https://www.dk-networks.de",
+    websiteText: "www.dk-networks.de",
+
+    address: [
+      "Robert-Bunsen-Straße 41",
+      "64579 Gernsheim, Germany"
+    ],
+
+    map:
+      "https://maps.google.com/?q=Robert-Bunsen-Straße+41+64579+Gernsheim+Germany",
+
+    qr: "/assets/employees/john-doe/qr.png",
+
+    vcf:
+      "/assets/employees/john-doe/john-doe.vcf"
   }
 
 };
